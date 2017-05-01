5b7b2f8a6a2b168d45128ed19ba3686da4d13fe9
Ramiro Balsa <ramiro.balsa@live.com>
2017-05-01 19:58:34 -0300

    Add files via upload

---
+++ Iteración.cpp
@@ -0,0 +1,25 @@
+/*Itineración
+*Ramiro Balsa
+*20170423
+*/
+
+using namespace std;
+
+#include <iostream>
+
+
+int main (){
+	
+	int i;
+	
+	
+	
+	for(int i=0; i<200; i++){
+		
+	std :: cout << "Debo estudiar progarmacion \n";
+	
+			
+	}
+				
+	
+}
---
+++ Saltos.cpp
@@ -0,0 +1,917 @@
+/*Saltos
+*Ramiro Balsa
+*20170423
+*/
+
+using namespace std;
+
+#include <istream>
+#include <iostream>
+
+
+
+int main (){
+    
+	string ingrese;
+	
+	     std :: cout << "Ingrese: debo estudiar programacion\n"; 
+	      
+         std :: getline (cin,ingrese);
+		 
+           
+          
+          
+          	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+       	   	 std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             std :: cout << ingrese << endl;
+             
+}
+         
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+         
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+                
+          
+          
+          
+          
+          
+             
+          
+          
+          
+          
+           
+          
+          
+          
+          
+          
+          
+          
+         
+		 
+		 
+		 
+		 
+		 		 
+		 
+		 
+		 
+		 
+		 	 
+		 
+		 
+		 
+		 
+		 
+		 			 
+		 
+		 
+	
+    
+ 
+ 	 		
+ 	
+ 	
+ 
+ 	
+ 
+ 	
+
+ 		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
